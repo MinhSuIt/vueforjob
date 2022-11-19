e5e4f1b63707694b24817e09e895f0ec75038315
minhsudoit <minhsudoit@gmail.com>
2022-11-19 22:54:59 +0700

    add ant design vue with vite

--- src/components/common/header.vue
+++ src/components/common/header.vue
@@ -1,15 +1,13 @@
 <template>
-    <div>
-        Header
-    </div>
+  <div>
+    Header
+    <a-button type="primary" danger>Integrate antd for vue with vite</a-button>
+  </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+export default {};
 </script>
 
 <style lang="scss" scoped>
-
 </style>
